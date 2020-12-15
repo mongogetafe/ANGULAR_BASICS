5e1545bde3ef33c5a6b6f752b012999c6a967aba
getafetest <getafetest@gmail.com>
2020-12-15 20:56:09 +0100

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { InterpolationComponent } from './interpolation/interpolation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    InterpolationComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/interpolation/interpolation.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-interpolation',
+  templateUrl: './interpolation.component.html',
+  styleUrls: ['./interpolation.component.scss']
+})
+export class InterpolationComponent implements OnInit {
+
+  actor = {
+    apellidos: 'de Niro',
+    nombre: 'Robert',
+    fechaNacimiento: new Date(1943, 7, 17)
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  getEdad() {
+    return Math.floor((new Date().getTime() - this.actor.fechaNacimiento.getTime()) / (365 * 24 * 60 * 60 * 1000))
+  }
+
+}
